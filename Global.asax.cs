7c4315fb82745b6683c0b84bd2e8ec634f602fef
Dejan Ostojic <dejanostojic@gmail.com>
2017-04-03 21:03:22 +0200

    Force HTTPS

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
---
+++ _Classes/Symmons/Custom/RedirectToHttps.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+using System.Configuration;
+using System.Web;
+
+namespace symmons.com._Classes.Symmons.Custom
+{
+    public class RedirectToHttps : HttpRequestProcessor
+    {
+        private HttpContext _currentContext = null;
+
+        public override void Process(HttpRequestArgs args)
+        {
+            _currentContext = args.Context;
+
+            if (!_currentContext.Request.IsSecureConnection && ForceHttps())
+            {
+                _currentContext.Response.Redirect(_currentContext.Request.Url.ToString().Replace("http:", "https:"));
+            }
+        }
+
+        private bool ForceHttps()
+        {
+            bool forceHttps = false;
+            bool forceHttpsTemp;
+
+            if (bool.TryParse(ConfigurationManager.AppSettings["ForceHttps"], out forceHttpsTemp))
+            {
+                forceHttps = forceHttpsTemp;
+
+                forceHttps = forceHttps && _currentContext.Request.Url.Host.Contains("symmons.com");
+            }
+
+            return forceHttps;
+        }
+    }
+}
